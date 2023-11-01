10b77f214c8f4d36079ad2aa536b1d30c9e3b1ea
Dan Liamco <dan.liamco@yahoo.com>
2023-11-01 20:48:59 +0800

    Update:  - Changed game over condition to only check once a Mochi has Landed

--- Assets/Scripts/Monobehaviors/Environment/GameOverConditionChecker.cs
+++ Assets/Scripts/Monobehaviors/Environment/GameOverConditionChecker.cs
@@ -6,9 +6,27 @@ public class GameOverConditionChecker : MonoBehaviour
 {
     [Header("Events")]
     [SerializeField] VoidEvent _gameOver;
+
+    List<Mochi> _mochisInArea = new List<Mochi>();
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<Mochi>())
+        if (collision.TryGetComponent<Mochi>(out Mochi mochi))
+        {
+            _mochisInArea.Add(mochi);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Mochi>(out Mochi mochi))
+        {
+            _mochisInArea.Remove(mochi);
+        }
+    }
+
+    public void OnMochiLanded()
+    {
+        if (_mochisInArea.Count > 0)
         {
             _gameOver.Raise();
         }
