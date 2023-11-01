ac8c664ec7aae5e75883990beab4a4e59d8dbdcc
Dan Liamco <dan.liamco@yahoo.com>
2023-11-01 21:28:00 +0800

    Update:  - Scoring is now based on value of Mochis inside play area, not calculated upon combinations

--- Assets/Scripts/Monobehaviors/Environment/PlayAreaZoneDetection.cs
+++ Assets/Scripts/Monobehaviors/Environment/PlayAreaZoneDetection.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayAreaZoneDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] MochiSet _mochisInPlayArea;
+
+    private void Start()
     {
-        
+        _mochisInPlayArea.items.Clear();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Mochi>(out Mochi mochi))
+        {
+            _mochisInPlayArea.Add(mochi);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.TryGetComponent<Mochi>(out Mochi mochi))
+        {
+            _mochisInPlayArea.Remove(mochi);
+        }
     }
 }
